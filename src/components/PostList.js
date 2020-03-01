263fdb59ef5c33fe42a2c09b6bc989e78931a6ef
MNANDO <mhernando334@gmail.com>
2020-03-01 02:48:43 -0500

    updated components

---
+++ src/components/PostList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class PostList extends React.Component {
+    render() {
+        const { posts }  = this.props
+
+        return (
+            <div className='postListContainer'>
+                {
+                    posts.map((_todo, _index) => {
+                        return (
+                        <div className="bg-primary w-50 mx-auto mb-3 p-5 rounded" key={_index}>{_todo}</div>
+                        )
+                    })
+                    
+                }
+            </div>
+        )
+    }
+}
+
+export default PostList
