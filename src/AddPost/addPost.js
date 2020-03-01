05f930fcc30e43e65a842809b81ce1e532447568
MNANDO <mhernando334@gmail.com>
2020-03-01 10:41:21 -0500

    howdy

--- src/AddPost/addPost.js
+++ src/AddPost/addPost.js
@@ -14,66 +14,61 @@ constructor() {
 
     render() {
         return (
-            <div className='addPostContainer mx-auto mb-5 mt-5 w-50'>
+            <div className='addPostContainer mx-auto mb-5 mt-5 w-50 bg-dark rounded p-5'>
                 <form
                  onSubmit={this.submitPost}>
-                     {/* <label htmlFor="cars">Description:</label>
-                    <input 
-                    id='addPostInput'
-                    type='text'
-                    onChange={(e) => this.updateInput(e)}>
-                    </input>  */}
                     <label>Zip Code:</label>
                     <input 
                     id='addZipInput'
                     type='number'
+                    className="zip-code bg-dark form-control w-25 text-light"
                     onChange={ this.handleZip }>
                     </input>
-                    <label>Choose food type:</label>
+                    <label
+                    className="w-100 pt-3"
+                    >Food type:</label>
+                    
                     <select
-                    value={this.state.fType}
-                    onChange={this.handleFood}
+                        className="selectpicker"
+                        value={this.state.fType}
+                        onChange={this.handleFood}
                     >
-                        <option value="Vegan">Vegan</option>
-                        <option value="Halal">Halal</option>
-                        <option value="Vegetarian">Vegetarian</option>
-                        <option value="Meat">Meat</option>
-                        <option value="Canned">Canned</option>
-                        <option value="Soup">Soup</option>
-                        <option value="Produce">Produce</option>
-                        <option value="Pastry">Pastry</option>
+                    <option value="Vegan">Vegan</option>
+                    <option value="Halal">Halal</option>
+                    <option value="Vegetarian">Vegetarian</option>
+                    <option value="Meat">Meat</option>
+                    <option value="Canned">Canned</option>
+                    <option value="Soup">Soup</option>
+                    <option value="Produce">Produce</option>
+                    <option value="Pastry">Pastry</option>
                     </select>
-                    <label>Quantity:</label>
-                    <select 
-                    value= {this.state.qty}
-                    onChange={this.handleQuantity}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                    </select>
-                    <label>Quantity:</label>
+                        
+                        <label
+                        className="w-100 pt-3"
+                        >Quantity:</label>
+                        <select 
+                        value= {this.state.qty}
+                        onChange={this.handleQuantity}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
                     <button
+                    className="btn btn-primary ml-5 w-50 ml-5" 
                     onClick={this.confirm}>
-                    Add food</button>
+                    Donate</button>
                     
                 </form>
-                <button onClick={localStorage.clear()}>clear all</button>
             </div>
         )
     }   
-
-    // updateInput = (e) => {
-    //     this.setState({
-    //         post: e.target.value
-    //     })
-    // }
    
     handleZip = (e) => {
 
@@ -84,7 +79,6 @@ constructor() {
     }
 
     handleFood = (e) => {                        
-            //console.log(this.state.fType);
             this.setState({
                 fType: e.target.value
             })
@@ -98,31 +92,15 @@ constructor() {
     }
 
     confirm = () => {
-        let postData = `Zip Code: ${this.state.zip} Meat: ${this.state.fType} Quantity: ${this.state.qty}`;
+        let postData = `Zip Code: ${this.state.zip} Food Type: ${this.state.fType} Quantity: ${this.state.qty}`;
 
-        //console.log(postData);
         this.setState({
             post: postData
         })
     }
 
-    /*
-    chatTextHandler = (text) => {
-  this.setState({ chatText : text});
-}
-
-    */
     submitPost = (e) => {
         
-        // let postData = `Zip Code: ${this.state.zip} Meat: ${this.state.fType} Quantity: ${this.state.qty}`;
-
-        // console.log(this.state)
-        // //console.log(postData);
-        // this.setState({
-        //     post: postData
-        // })
-
-        //console.log(this.state)
 
         this.confirm();
         
